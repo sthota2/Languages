7bb7d495b9d6ed55cb08b20a05c07ffa31062366
U-DESKTOP-71KFD3D\sandeep <tsandyk8s@gmail.com>
2022-05-02 08:56:45 -0500

    First Commit

---
+++ Python/tuples/test.py
@@ -0,0 +1,12 @@
+my_tuple = (1, 10, 100, 1000)
+
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple[1:])
+print(my_tuple[:-2])
+
+for elem in my_tuple:
+    print(elem)
+#my_tuple.append(10000)
+#del my_tuple[0]
+#my_tuple[1] = -10
