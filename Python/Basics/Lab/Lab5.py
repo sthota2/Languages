a047e63fee8ad8126b346f1d0bf55aae8e4d58f7
tsandyk8s@gmail.com <tsandyk8s@gmail.com>
2023-03-28 21:42:20 -0500

    Labs 5

---
+++ Python/Basics/Lab/Lab5.py
@@ -0,0 +1,7 @@
+john = 3
+mary = 5
+adam = 6
+print(str(john)+','+str(mary)+','+str(adam))
+total_apples = john + mary + adam
+print(total_apples)
+print('Total Number of apples:'+str(total_apples))
