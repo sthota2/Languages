c196b1edb7082ea80156956d678267d8e7e81c65
tsandyk8s@gmail.com <tsandyk8s@gmail.com>
2023-03-29 08:01:24 -0500

    Labs 6

---
+++ Python/Basics/Lab/Lab6.py
@@ -0,0 +1,9 @@
+kilometers = 12.25 # provided
+miles = 7.38 # provided
+
+miles_to_kilometers = miles * 1.6093 # Answer
+kilometers_to_miles = kilometers * 0.6213 # Answer
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers") # provided
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles") # provided
+
