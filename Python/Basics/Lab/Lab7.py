20ed4bc40011d9efce767c6bd9656f57f0c927d0
tsandyk8s@gmail.com <tsandyk8s@gmail.com>
2023-03-31 07:54:21 -0500

    lab 9

---
+++ Python/Basics/Lab/Lab7.py
@@ -0,0 +1,5 @@
+x =  -1 # Test the Data
+x = float(x)
+# write your code here
+y = 3 * (x ** 3) - 2 * (x ** 2) + (3 * x) -1 # Solution
+print("y =", y)
---
+++ Python/Basics/Lab/Lab8.py
@@ -0,0 +1,9 @@
+# this program computes the number of seconds in a given number of hours
+hours = 2
+seconds_per_hour = 3600
+
+print("Hours: ", hours)
+print("Seconds in Hours: ", hours * seconds_per_hour)
+print('Good Bye')
+print('Number of Seconds in 3 hrs',3 * seconds_per_hour)
+git
