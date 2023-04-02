efa14b1ae51cdaf86d3cc4e33f9bc9fe35196b54
tsandyk8s@gmail.com <tsandyk8s@gmail.com>
2023-04-01 22:45:21 -0500

    Using Operatiors

--- Python/Basics/Labs11-20/Lab11.py
+++ Python/Basics/Labs11-20/Lab11.py
@@ -3,12 +3,6 @@ mins = int(input("Starting time (minutes): "))
 dura = int(input("Event duration (minutes): "))
 
 # Write your code here.
-converted_hour = int((mins / 60 + dura / 60) % 24)
-total_hour = hour + converted_hour
-
-if total_hour > 23:
-    total_hour = hour + converted_hour - 24
-
-converted_min = hour * 60
-total_min = (converted_min + mins + dura) % 60
-print(str(total_hour) + ':' + str(total_min))
+total_hours = int((hour + (mins / 60) + (dura / 60)) % 24)
+total_mins = (hour * 60 + mins + dura) % 60
+print(total_hours, ":", total_mins, sep="")
