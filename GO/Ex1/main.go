51bcd25de7e3e410f9b645373bfd8cd6eb83227b
tsandyk8s@gmail.com <tsandyk8s@gmail.com>
2025-01-12 21:16:05 -0600

    go testing

---
+++ GO/Ex1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this is comment
+func main() {
+	/* this is mutli-line comment comment */
+	fmt.Println("Hello, World!")
+	fmt.Println("Hello, Deepni & Ishaan")
+	fmt.Print("Hello, Deepni & Ishaan")
+	fmt.Printf("Hello, Deepni & Ishaan")
+}
