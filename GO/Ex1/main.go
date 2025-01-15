c234b718116ababaed5d60111dc62463b83a34eb
tsandyk8s@gmail.com <tsandyk8s@gmail.com>
2025-01-15 07:30:28 -0600

    excerise 1

--- GO/Ex1/main.go
+++ GO/Ex1/main.go
@@ -6,7 +6,21 @@ import "fmt"
 func main() {
 	/* this is mutli-line comment comment */
 	fmt.Println("Hello, World!")
-	fmt.Println("Hello, Deepni & Ishaan")
-	fmt.Print("Hello, Deepni & Ishaan")
-	fmt.Printf("Hello, Deepni & Ishaan")
+	fmt.Println("Hello, Nanu & Kitty")
+	fmt.Print("Hello, Nanu & Kitty")
+	fmt.Printf("\nHello, Nanu & Kitty")
+
+	// Variable declaration
+	var name string = "Nanu"
+	fmt.Println(name)
+	var age int = 8
+	fmt.Println(age)
+	var isCool bool = true
+	fmt.Println(isCool)
+	var weight float32 = 30.5
+	fmt.Println(weight)
+
+	// Formating in go is done using prinf
+	fmt.Printf("My name is %v and I am %d years old", name, age)
+
 }
